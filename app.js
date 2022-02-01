247e3f2c3fd78fb1ac99b2447ffd2a222e5d7dc9
unknown <tracznico91@gmail.com>
2022-02-01 11:56:55 -0300

    Cambios en app.js(linea 7) para que funcione en Heroku.

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const app = express();
 app.use(express.static('public'));
 
 
-app.listen(3000, ()=>{
-    console.log('Servidor funcionando');
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log('Servidor funcionando en puerto 3000');
 });
 
 app.get('/', (req,res)=>{
